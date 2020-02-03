c8583d72d286051cfd7169215cf002ee1e36364a
Jan Jongboom <janjongboom@gmail.com>
2020-02-03 10:33:35 +0100

    Should use 'timestamp' as type

--- edge-impulse-parquet/index.d.ts
+++ edge-impulse-parquet/index.d.ts
@@ -2,11 +2,7 @@
 // This file is far from complete, but at least good enough for our simple usecase
 
 export type ParquetType = 'boolean' | 'int32' | 'int64' | 'int96' | 'float' | 'double' |
-                          'byte_array' | 'fixed_len_byte_array' | 'utf8' | 'map' | 'map_key_value' |
-                          'list' | 'enum' | 'decimal' | 'date' | 'time_millis' | 'time_micros' |
-                          'timestamp_millis' | 'timestamp_micros' | 'uint_8' | 'uint_16' | 'uint_32' |
-                          'uint_64' | 'int_8' | 'int_16' | 'int_32' | 'int_64' | 'json' | 'bson' |
-                          'interval';
+                          'byte_array' | 'timestamp' | 'utf8';
 
 export type ParquetSchema = { [k: string]: { type: ParquetType, optional?: boolean } };
 export type ParquetRows = ((number | string | boolean | undefined)[] | Buffer | string)[];
